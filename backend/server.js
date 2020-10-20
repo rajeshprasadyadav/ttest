aba04a5f6987ac80b02b9d875c7cbd45f5d753b1
Rajesh Prasad Yadav <rpy31186@gmail.com>
2020-10-20 19:36:59 +0530

    configuring end point

--- backend/server.js
+++ backend/server.js
@@ -5,50 +5,56 @@ const qs = require('qs');
 const axios = require('axios');
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
+
 /**
- * APP ID === Client Id
+ * APP ID,Unique for each application, we create in (portal.azure.com);
+ * We can co-realte it with each partner/cohort 
+ * at run time these credentials will be pulled from database
  */
 const APP_ID = "232181dd-d281-40f0-8c19-37234553bd57";
 /**
- * Use one tannent we can invoke as many application
+ * Unique for each Microsoft account,basically unique for service provider
+ * Tenant_id is required for getting token,irrespective of APP_ID.
  */
 const TENANT_ID="98d53658-2931-4186-ac56-42a6b796e78b";
 /**
- * 
+ * Secret is required to get token,each application will have secret key as well
  */
 const SECRET="TM1NA9Oy7FmvLje~.Wl0FavyRO1_-m0..l";
 /**
- * constant throughout
+ * this is constant and required for getting token
  */
 const SCOPE = "https://graph.microsoft.com/.default";
 
 
 
 /**
- * OBJECT_ID can be any user who is in application active direcory
- * It is basically an User wpplication will create an online event on behalf of that.
- * 
+ * Object ID , represent any activie user under Azure Active Directory
+ *
  */
 const OBJECT_ID="890d2bbc-5097-4688-97c4-742b5ebb5dcc";
 
+//another user id === ObjectID
+//const OBJECT_ID="f89fdfa5-bec2-421b-a9ef-b0016f5bcb09";
+
 /**
  * 
- * Const througout
+ * Constant required for get access_token
  */
 
- const GRANT_TYPE="client_credentials";
+const GRANT_TYPE="client_credentials";
 
- /**
-  *  Token URL 
-  */
+/**
+ * End point for getting access_token,
+ * Once We will have access token we can hit the end for for Creating Event.
+ * Required Field: app_id,secret,scope & grant_type , (last two fields is constant)
+ */
+const TOKEN_URL = "https://login.microsoftonline.com/"+TENANT_ID+"/oauth2/v2.0/token";
 
-  const TOKEN_URL = "https://login.microsoftonline.com/"+TENANT_ID+"/oauth2/v2.0/token";
-
-  /**
-   * JOIN Link URL
-   */
-
-  const JOIN_LINK_URL="https://graph.microsoft.com/v1.0/users/"+OBJECT_ID+"/calendar/events";
+/**
+ * 
+ */
+const JOIN_LINK_URL="https://graph.microsoft.com/v1.0/users/"+OBJECT_ID+"/calendar/events";
 
 let token="";
 
