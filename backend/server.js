440b4499dedf22935fa520dce438a9fbe285ebc5
Rajesh Prasad Yadav <rpy31186@gmail.com>
2020-10-20 18:43:21 +0530

    creating endpoint

--- backend/server.js
+++ backend/server.js
@@ -47,7 +47,7 @@ const OBJECT_ID="890d2bbc-5097-4688-97c4-742b5ebb5dcc";
    * JOIN Link URL
    */
 
-   const JOIN_LINK_URL="https://graph.microsoft.com/v1.0/users/"+OBJECT_ID+"/calendar/events";
+  const JOIN_LINK_URL="https://graph.microsoft.com/v1.0/users/"+OBJECT_ID+"/calendar/events";
 
 let token="";
 
@@ -71,9 +71,6 @@ async function getToken(){
 }
 async function getJoinLink(token){
     try{
-        //token = "Bearer "+token;
-        console.log(token );
-        console.log(JOIN_LINK_URL);
         return await axios.post(
             JOIN_LINK_URL,
             {
@@ -118,46 +115,6 @@ async function getJoinLink(token){
         console.log(err);
     }
 
-    /*try{
-        return await axios.post(JOIN_LINK_URL,{subject: "Let's go for lunch",
-        body: {
-            contentType: "HTML",
-            content: "Does noon work for you?"
-        },
-        start: {
-            dateTime: "2020-10-20T19:00:00",
-            timeZone: "Asia/Kolkata"
-            },
-        end: {
-            dateTime: "2020-10-20T19:45:00",
-            timeZone: "Asia/Kolkata"
-        },
-        location:{
-            displayName:"Coaching Session"
-        },
-        attendees: [
-                {
-                emailAddress: {
-                    address :"rajeshy@ispace.com",
-                    name: "Rajesh"
-                },
-                type: "required"
-                }
-            ],
-        allowNewTimeProposals: true,
-        isOnlineMeeting: true,
-        onlineMeetingProvider: "teamsForBusiness"
-            
-    },{headers:{
-        'Authorization':'Bearer '+token,
-        'Content-Type':'application/json'
-    }})
-    }catch(err){
-        console.log(err);
-    }*/
-    
-    
-    
 }
 
 
@@ -181,11 +138,13 @@ app.get('/getMeetingLink',(req,res)=>{
             //console.log(token);
             getJoinLink(token).then(res=>{
                 if(res.status===201){
+                    res.status(201).send(res.data);
                     console.log('Created Succesfully');
+                }else if(res.status===401){
+                   
                 }
-                console.log("============ Response ============")
-                console.log(res);
-                console.log("============ Response end================");
+
+               
             }).catch(err=>{
                 console.log("Request error =============");
                 console.log(err)
@@ -196,11 +155,6 @@ app.get('/getMeetingLink',(req,res)=>{
    
 }
 )
-
-app.get("/test",(req,res)=>{
-    res.status(200).json({"message":"request successfull"});
-})
-
 app.listen(8000,()=>{
     console.log("Server started on port 8000")
 })
