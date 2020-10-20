af1df9868c2a3683fbc0af8205c9db79bfafb3b0
Rajesh Prasad Yadav <rpy31186@gmail.com>
2020-10-20 15:52:18 +0530

    Configuring servergit status !

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,42 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+/**
+ * APP ID === Client Id
+ */
 const APP_ID = "232181dd-d281-40f0-8c19-37234553bd57";
+/**
+ * Use one tannent we can invoke as many application
+ */
 const TENANT_ID="98d53658-2931-4186-ac56-42a6b796e78b";
+/**
+ * 
+ */
 const SECRET="TM1NA9Oy7FmvLje~.Wl0FavyRO1_-m0..l";
+/**
+ * 
+ */
+const SCOPE = "https://graph.microsoft.com/.default";
+
+/**
+ * 
+ */
+
+ const GRANT_TYPE="client_credentials";
+
 
 /**
  * OBJECT_ID can be any user who is in application active direcory
  * 
  */
 const OBJECT_ID="890d2bbc-5097-4688-97c4-742b5ebb5dcc"
+let token="";
 
 function getToken(){
 
 }
 
-function getTokenExistingExpired(){
 
-}
 
 app.get('/getMeetingLink',(req,res)=>{
 
